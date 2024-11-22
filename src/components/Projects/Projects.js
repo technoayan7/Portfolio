40659d702f9c5b00ef31bad7247ff088686dfa5a
technoayan <ayanahmad7052@gmail.com>
2024-11-23 01:04:16 +0530

    Added Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import derain from "../../Assets/Projects/derain.jpg";
+import job from "../../Assets/Projects/job.jpg";
+import mealdash from "../../Assets/Projects/mealdash.jpg";
 import tictac from "../../Assets/Projects/tictac.jpg";
 import FitHub from "../../Assets/Projects/fithub.jpg";
 import MoviePulse from "../../Assets/Projects/MoviePulse.jpg";
@@ -24,34 +24,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={job}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="CareerHorizon"
+              description="CareerHorizon is a fully functional job portal that manages real-time job listings and provides secure authentication for users. It features dynamic job search filters, application tracking, and recruiter management, significantly improving search efficiency and user experience."
+              ghLink="https://github.com/technoayan7/CareerHorizon"
+              demoLink="https://careerhorizon.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={mealdash}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="MealDash"
+              description="MealDash is a dynamic food delivery platform designed with features like category browsing, cart management, an admin dashboard for managing food items, and seamless light/dark mode switch. It integrates secure payment processing, enhancing user experience and accessibility while streamlining transactions."
+              ghLink="https://github.com/technoayan7/mealdash"
+              demoLink="https://mealdash-ui.onrender.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={derain}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              title="Restormer"
+              description="Restormer is an advanced transformer-based model designed for high-resolution image restoration, particularly for rain removal tasks. Using a custom dataset of rain/ground truth image pairs, it achieved enhanced efficiency and image quality, surpassing previous benchmarks in PSNR and SSIM performance."
+              ghLink="https://github.com/technoayan7/Restormer"
+              demoLink="https://colab.research.google.com/drive/1lOzh9Rpz4cdLput6Y00sU4tNuZJTtjBY#scrollTo=SRd46QaXlklQ"
             />
           </Col>
 
@@ -60,7 +60,7 @@ function Projects() {
               imgPath={MoviePulse}
               isBlog={false}
               title="MoviePulse"
-              description="This application provides all the details of the requested movie such as overview, genre, release date, rating, runtime, top cast, Sortby, reviews, recommended movies, etc."
+              description="MoviePulse is a feature-rich application that provides detailed information about movies, including an overview, genre, release date, rating, runtime, top cast, reviews, sorting options, and personalized recommendations."
               ghLink="https://github.com/technoayan7/MoviePulse"
               demoLink="https://moviepulse.netlify.app/"
             />
@@ -71,7 +71,7 @@ function Projects() {
               imgPath={FitHub}
               isBlog={false}
               title="FitHub"
-              description="Developed a FitHub Exercise website using Rapid Api."
+              description="Developed a responsive fitness website offering categorized exercise information and YouTube workout videos via a REST API. Integrated React Router for smooth navigation and pagination for efficient content browsing, paired with a visually engaging interface using Material UI."
               ghLink="https://github.com/technoayan7/FitHub"
               demoLink="https://fit-hub.vercel.app/"
             />
@@ -82,7 +82,7 @@ function Projects() {
               imgPath={Objectdetection}
               isBlog={false}
               title="Image Dehazing"
-              description="Python implementation of the paper: Efficient Image Dehazing with Boundary Constraint and Contextual Regularization and its Object Detection."
+              description="Implementation of the Efficient Image Dehazing with Boundary Constraint and Contextual Regularization paper, focusing on enhancing image clarity. Extended the project to include object detection, leveraging improved visibility for accurate detection results."
               ghLink="https://github.com/technoayan7/Object-Detection-in-an-Hazy-Image"
               demoLink="https://github.com/technoayan7/Object-Detection-in-an-Hazy-Image"
             />
