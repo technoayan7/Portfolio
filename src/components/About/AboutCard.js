fd9ba4e0eb1b21219b086596d15811fc5a53d4c3
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-06-28 04:16:35 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,27 +1,30 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-// import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className="quote-card-view about-card">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
+          <p className="about-text">
             Hi Everyone, I am <span className="purple">Ayan Ahmad. </span>
-            <br /> I am a Third-year undergrad at <span className="purple">IIIT Allahabad</span> pursuing a Bachelor of Technology in Information Technology.
-            I am highly interested in algorithmic problem-solving and have a firm grasp in data structures and algorithms. In my one year of experience of practicing competitive programming,
-            I have climbed up to the Specialist at Codeforces ( Solved Over More than 600 problems ) and a 4🌟 at Codechef.
             <br />
-            I am Web Development enthusiast and have made quite a few Projects in React Js and Firebase. I am currently learning more about MongoDB, Express, React, Node.
+            I am a Final-year undergrad at <span className="purple">IIIT Allahabad</span> pursuing a Bachelor of Technology in Information Technology.
             <br />
-
+            <br />
+            I am highly interested in <span className="highlight">algorithmic problem-solving</span> and have a firm grasp in data structures and algorithms. In my one year of experience of practicing competitive programming,
+            I have climbed up to the <span className="achievement">Expert at Codeforces</span> ( Solved Over More than 700 problems ) and a <span className="achievement">5🌟 at Codechef</span>.
+            <br />
+            <br />
+            I am <span className="highlight">Web Development enthusiast</span> and have made quite a few Projects in React Js and Firebase. I am currently learning more about Go, MongoDB, Express, React, Node.
           </p>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Everthing Thats kills Me, Makes Me feel Alive!"{" "}
+          <p className="quote-text">
+            "Everything That's Kills Me, Makes Me Feel Alive!"
           </p>
-          <footer className="blockquote-footer">Ayan</footer>
+          <footer className="blockquote-footer">
+            <cite title="Source Title">Ayan</cite>
+          </footer>
         </blockquote>
       </Card.Body>
     </Card>
